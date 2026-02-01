695064390d764f4583848ac4b539d026efd2c00c
Mourad Sehboub <139013207+MouradWPF@users.noreply.github.com>
2026-02-01 13:53:35 -0500

    Initialize project using Create React App
    # Conflicts:
    #	package-lock.json
    #	package.json
    #	src/App.css
    #	src/App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Sommaire from "./Sommaire";
-import ReactGuide from "./ReactGuide"; // Ton composant guide complet
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Sommaire />} />
-                <Route path="/GuideReact" element={<ReactGuide />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
